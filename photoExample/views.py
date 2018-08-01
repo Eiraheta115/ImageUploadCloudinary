0be1f2b538a6641705e5adcdb077534202f0df04
Emerson Iraheta <ceiraheta87@gmail.com>
2018-08-01 12:48:38 -0600

    adding explination

--- photoExample/views.py
+++ photoExample/views.py
@@ -12,8 +12,16 @@ class photoExampleviewSet(viewsets.ModelViewSet):
 
     @action(methods=['post'], detail=False)
     def uploadImage(self, request):
+        #Gets json from request body
         data = json.loads(request.body)
+        """
+        this is a post request to upload a photo to cloudinary
+        converting a base64 string that contains the photo 
+        and saves the response from the post
+        """
         respon= cloudinary.uploader.upload(data["photo"])
+        #Save in table the URL from the upload 
         p = photoExample.objects.create(url=respon["url"])
+        #Send message of success
         content = {'message': 'image uploaded and saved', 'photo': p.url}
         return Response(content, status=status.HTTP_201_CREATED)
